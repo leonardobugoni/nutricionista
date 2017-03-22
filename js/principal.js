362a9ee2ccc0a0230d0aac3be0e5ec9326093ea4
leooferreiraa <leoferreira@unochapeco.edu.br>
2017-03-22 12:59:43 -0300

    Finalizando botao

--- js/principal.js
+++ js/principal.js
@@ -39,6 +39,39 @@ for (var i = 0; i< pacientes.length; i++){
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click" , function(){
-	console.log("Oi, cliquei no botao");
+botaoAdicionar.addEventListener("click" , function(){ //Cria um evento de click chamando uma function
+	event.preventDefault(); //Evita o comportamento padrao de um evento
+	
+	var form = document.querySelector("#form-adiciona");
+	
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var alutra = form.altura.value;
+	var gordura = form.gordura.value;
+
+	var pacienteTr = document.createElement("tr");
+
+	var nomeTd = document.createElement("td"); //Cria uma variavel e Cria um elemento Td (tabela) no codigo
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	nomeTd.textContent = nome; //Seleciona o conteudo do texto e liga com a variavel
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura
+
+	pacienteTr.appendChild(nomeTd);//Adiciona como filho de outra função
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+	
+	console.log(pacienteTr);
+
+	var tabela = document.querySelector("#tabela-pacientes");
+
+	tabela.appendChild(pacienteTr);
+
+
 });
