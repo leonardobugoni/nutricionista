8095e8a2c2276a0f993ce43b6f33e2c770a23ea5
leooferreiraa <leoferreira@unochapeco.edu.br>
2017-03-22 11:38:29 -0300

    Calculando todos os IMC, e aplicando estilos

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,41 @@
 var titulo = document.querySelector(".titulo"); //CRIA VARIAVEL E LINKA COM O "TITULO"
 titulo.textContent = "Aparecida	Nutricionista"; //MUDA O CONTEUDO DO TEXTO
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso  = tdPeso.textContent;
+for (var i = 0; i< pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+	var paciente = pacientes[i]
 
-var tdImc = paciente.querySelector(".info-imc");
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso  = tdPeso.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoValido = true;
+	var alturaValida = true;
+
+
+	if (peso <= 0 || peso >= 1000) {
 	console.log("Peso Invalido");
 	pesoValido = false;
 	tdImc.textContent = "Peso Invalido";
-}
-if (altura <= 0 || altura >= 3.00){
+	paciente.classList.add("paciente-invalido");
+	}
+	if (altura <= 0 || altura >= 3.00){
 	console.log("Altura Invalida");
 	alturaValida = false; 
 	tdImc.textContent = "altura Invalida";
-}
-if (alturaValida && pesoValido){
+	paciente.classList.add("paciente-invalido");
+	}
+	if (alturaValida && pesoValido){
 	var imc = peso / (altura * altura);
-	tdImc.textContent = imc;
+	tdImc.textContent = imc.toFixed(2);
 }
+
+}
+
+
