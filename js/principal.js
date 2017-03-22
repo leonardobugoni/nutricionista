76b5ea1e4584896d530f1e309e93b618e8663711
leooferreiraa <leoferreira@unochapeco.edu.br>
2017-03-22 15:48:05 -0300

    Resolvendo bug do NOME

--- js/principal.js
+++ js/principal.js
@@ -32,46 +32,15 @@ for (var i = 0; i< pacientes.length; i++){
 	paciente.classList.add("paciente-invalido");
 	}
 	if (alturaValida && pesoValido){
-	var imc = peso / (altura * altura);
-	tdImc.textContent = imc.toFixed(2);
-	}	
+	var imc = calculaImc(peso, altura);
+	tdImc.textContent = imc;
+	}
 
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click" , function(){ //Cria um evento de click chamando uma function
-	event.preventDefault(); //Evita o comportamento padrao de um evento
-	
-	var form = document.querySelector("#form-adiciona");
-	
-	var nome = form.nome.value;
-	var peso = form.peso.value;
-	var alutra = form.altura.value;
-	var gordura = form.gordura.value;
+function calculaImc(peso,altura){
+	var imc = 0;
+	imc = peso / (altura * altura);
 
-	var pacienteTr = document.createElement("tr");
-
-	var nomeTd = document.createElement("td"); //Cria uma variavel e Cria um elemento Td (tabela) no codigo
-	var pesoTd = document.createElement("td");
-	var alturaTd = document.createElement("td");
-	var gorduraTd = document.createElement("td");
-	var imcTd = document.createElement("td");
-
-	nomeTd.textContent = nome; //Seleciona o conteudo do texto e liga com a variavel
-	pesoTd.textContent = peso;
-	alturaTd.textContent = altura;
-	gorduraTd.textContent = gordura
-
-	pacienteTr.appendChild(nomeTd);//Adiciona como filho de outra função
-	pacienteTr.appendChild(pesoTd);
-	pacienteTr.appendChild(alturaTd);
-	pacienteTr.appendChild(gorduraTd);
-	
-	console.log(pacienteTr);
-
-	var tabela = document.querySelector("#tabela-pacientes");
-
-	tabela.appendChild(pacienteTr);
-
-
-});
+	return imc.toFixed(2);
+}
